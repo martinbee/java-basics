303bbadc5aa0e802a440490d66e6074b0b1663aa
Martin Bee <martbee88@gmail.com>
2017-09-05 11:14:13 -0500

    linked-lists: add song class

---
+++ lessons/linked-lists/Song.java
@@ -0,0 +1,11 @@
+public class Song {
+  private String title;
+  private double duration;
+
+  public Song(String title, double duration) {
+    this.title = title;
+    this.duration = duration;
+  }
+}
+
+// Create a Song class having Title and Duration for a song.
